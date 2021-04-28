176caba835bb59dcf6485993f70dab80878d8cb6
Miguel Malagon <miguelmalagon@outlook.com>
2021-04-27 20:38:51 -0500

    lasta changes

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,6 +1,8 @@
 export default {
 	mongoUrl: {
-		dev: 'mongodb://localhost/ecb',
-		prod: 'mongodb://localhost/dev'
+		dev:
+			'mongodb+srv://root:r7fb4XGNMREzC8H@cluster0.cnubs.mongodb.net/ecb?retryWrites=true&w=majority',
+		prod:
+			'mongodb+srv://root:r7fb4XGNMREzC8H@cluster0.cnubs.mongodb.net/ecb?retryWrites=true&w=majority'
 	}
 };
