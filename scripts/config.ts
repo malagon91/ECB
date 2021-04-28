48fda26639613de8e13e0a26d27e8ee4fd24d294
Miguel Malagon <miguelmalagon@outlook.com>
2021-04-27 22:36:42 -0500

    update password

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,8 +1,8 @@
 export default {
 	mongoUrl: {
 		dev:
-			'mongodb+srv://root:r7fb4XGNMREzC8H@cluster0.cnubs.mongodb.net/ecb?retryWrites=true&w=majority',
+			'mongodb+srv://root:password@cluster0.cnubs.mongodb.net/ecb?retryWrites=true&w=majority',
 		prod:
-			'mongodb+srv://root:r7fb4XGNMREzC8H@cluster0.cnubs.mongodb.net/ecb?retryWrites=true&w=majority'
+			'mongodb+srv://root:password@cluster0.cnubs.mongodb.net/ecb?retryWrites=true&w=majority'
 	}
 };
